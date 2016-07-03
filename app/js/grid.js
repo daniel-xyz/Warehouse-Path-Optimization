e237bcac77a51bf86b4d00aa45080405f384ffeb
Daniel Bischoff <danielbischoff@hotmail.com>
2016-07-03 20:41:43 +0200

    Items werden jetzt zufällig den Slots zugeordnet und gerendert

--- app/js/grid.js
+++ app/js/grid.js
@@ -1,32 +1,43 @@
 var grid = (function($) {
 
   var lanes = 7,
-      itemsOnLane = 12,
+      slotsInLane = 12,
+      itemsTotal = lanes * slotsInLane,
       itemPixelSize = 20,
-      waypoints = new Array(lanes);
+      waypoints = new Array(lanes),
+      itemPositions = {
+        'Äpfel': getRandomPosition(),
+        'Birnen': getRandomPosition(),
+        'Salate': getRandomPosition(),
+        'Bananen': getRandomPosition(),
+        'Melonen': getRandomPosition()
+      };
 
   this.init = function() {
-    for (var lane = 1; lane <= lanes; lane++) {
-      waypoints[lane] = new Array(itemsOnLane);
-
-      for (var item = 1; item <= itemsOnLane; item++) {
-        waypoints[lane][item] = item;
-      }
-    }
-
+    generateWaypoints();
     renderGrid();
   };
 
-  function renderGrid() {
+  function generateWaypoints() {
     for (var lane = 1; lane <= lanes; lane++) {
-      for (var item = 1; item <= itemsOnLane; item++) {
+      waypoints[lane] = new Array(slotsInLane);
 
-        var dynamicSelector = lane + "-" + item;
+      for (var slot = 1; slot <= slotsInLane; slot++) {
+        waypoints[lane][slot] = slot;
+      }
+    }
+  }
+
+  function renderGrid() {
+    for (var lane = 1; lane < waypoints.length; lane++) {
+      for (var slot = 1; slot < waypoints[lane].length; slot++) {
+
+        var dynamicSelector = lane + "-" + slot;
 
         // Waypoint
         $('<div id=' + "waypoint-" + dynamicSelector + '/>').css({
           'position': 'absolute',
-          'top': (item * itemPixelSize) + item * 3,
+          'top': (slot * itemPixelSize) + slot * 3,
           'margin-left': (3 * lane * itemPixelSize) + (5 * lane) - (2 * itemPixelSize),
           'width': itemPixelSize,
           'height': itemPixelSize,
@@ -35,8 +46,8 @@ var grid = (function($) {
           'border-style': 'solid'
         }).appendTo('.grid-container');
 
-        // left Item
-        $('<div id=' + "item-left-" + dynamicSelector + '/>').css({
+        // Left Item Slot
+        $('<div id=' + "slot-left-" + dynamicSelector + '/>').css({
           'position': 'absolute',
           'top': 0,
           'right': itemPixelSize,
@@ -45,10 +56,10 @@ var grid = (function($) {
           'color': '#00BFFF',
           'border': '1px',
           'border-style': 'solid'
-        }).appendTo('#' + 'waypoint-' + dynamicSelector);
+        }).appendTo('#waypoint-' + dynamicSelector);
 
-        // Right Item
-        $('<div id=' + "item-right-" + dynamicSelector + '/>').css({
+        // Right Item Slot
+        $('<div id=' + "slot-right-" + dynamicSelector + '/>').css({
           'position': 'absolute',
           'top': 0,
           'left': itemPixelSize,
@@ -57,9 +68,49 @@ var grid = (function($) {
           'color': '#00BFFF',
           'border': '1px',
           'border-style': 'solid'
-        }).appendTo('#' + 'waypoint-' + dynamicSelector);
+        }).appendTo('#waypoint-' + dynamicSelector);
       }
     }
+
+    for(var item in itemPositions) {
+      if (itemPositions.hasOwnProperty(item)) {
+
+        var laneNr = getLaneNr(itemPositions[item]);
+        var slotNr = getSlotNr(itemPositions[item]);
+        var availableSlots = ['right', 'left'];
+        var randomSlot = availableSlots[Math.floor(Math.random() * (1 + 1))];
+        var position = laneNr + "-" + slotNr;
+
+        $('<div id=' + "item-" + position + '/>').css({
+          'position': 'absolute',
+          'top': 2,
+          'left': 2,
+          'width': itemPixelSize - 6,
+          'height': itemPixelSize - 6,
+          'color': '#3CB371',
+          'background-color': '#3CB371',
+          'border': '1px',
+          'border-style': 'solid',
+          'border-radius': 30
+        }).appendTo('#slot-' + randomSlot + '-' + position);
+      }
+    }
+  }
+
+  function getLaneNr(absolutePosition) {
+    return Math.ceil(absolutePosition / slotsInLane);
+  }
+
+  function getSlotNr(absolutePosition) {
+    if (absolutePosition % slotsInLane === 0) {
+      return slotsInLane;
+    } else {
+      return absolutePosition % slotsInLane;
+    }
+  }
+
+  function getRandomPosition() {
+    return Math.floor(Math.random() * (itemsTotal - 1) + 1);
   }
 
   return this;
