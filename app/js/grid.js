6ac45ee891eec231fceeab4ba0e0837da17ec96f
Daniel Bischoff <danielbischoff@hotmail.com>
2016-07-04 20:18:23 +0200

    Nutzung von prototypes in grid.js

--- app/js/grid.js
+++ app/js/grid.js
@@ -1,120 +1,115 @@
-var grid = (function($) {
+var grid = new Grid(7, 24),
+    testJobs = {
+      1: [19, 38 , 5],
+      2: [130, 60, 40],
+      3: [94, 49, 146]
+    };
 
-  var lanes = 7,
-      slotsInLane = 12,
-      itemsTotal = lanes * slotsInLane,
-      itemPixelSize = 20,
-      waypoints = new Array(lanes),
-      itemPositions = ['Äpfel', 'Birnen', 'Salate', 'Bananen', 'Melonen'];
+function Grid(lanes, slotsInLane) {
+  this.lanes = lanes;
+  this.slotsInLane = slotsInLane;
+  this.slotPixelSize = 20;
+}
 
-  this.init = function() {
-    generateWaypoints();
-    setItemPositions();
-    renderGrid();
-  };
+Grid.prototype.getLanes = function() {
+  return this.lanes;
+};
 
-  function generateWaypoints() {
-    for (var lane = 1; lane <= lanes; lane++) {
-      waypoints[lane] = new Array(slotsInLane);
+Grid.prototype.getSlotsInLane = function() {
+  return this.slotsInLane;
+};
 
-      for (var slot = 1; slot <= slotsInLane; slot++) {
-        waypoints[lane][slot] = slot;
-      }
-    }
-  }
+Grid.prototype.getSlotPixelSize = function() {
+  return this.slotPixelSize;
+};
 
-  function setItemPositions() {
-    for(var item in itemPositions) {
-      if (itemPositions.hasOwnProperty(item)) {
-        itemPositions[item] = getRandomPosition();
-      }
-    }
-  }
+Grid.prototype.getSlotNr = function(lane, slot) {
+  return ((lane  - 1) * this.slotsInLane) + slot;
+};
+
+Grid.prototype.getWaypointNr = function(lane, slot) {
+  return Math.ceil(grid.getSlotNr(lane, slot) / 2);
+};
+
+Grid.prototype.getLaneNr = function(slotNr) {
+  return Math.ceil(slotNr / this.slotsInLane);
+};
+
+Grid.prototype.render = function() {
+
+  renderGrid();
+  renderItems();
 
   function renderGrid() {
-    for (var lane = 1; lane < waypoints.length; lane++) {
-      for (var slot = 1; slot < waypoints[lane].length; slot++) {
+    for (var lane = 1; lane <= grid.getLanes(); lane++) {
+      for (var slot = 1; slot <= grid.getSlotsInLane(); slot++) {
 
-        var dynamicSelector = lane + "-" + slot;
+        var size = grid.getSlotPixelSize();
+        var waypointNr = grid.getWaypointNr(lane, slot);
+        var slotNr = grid.getSlotNr(lane, slot);
 
-        // Waypoint
-        $('<div id=' + "waypoint-" + dynamicSelector + '/>').css({
-          'position': 'absolute',
-          'top': (slot * itemPixelSize) + slot * 3,
-          'margin-left': (3 * lane * itemPixelSize) + (5 * lane) - (2 * itemPixelSize),
-          'width': itemPixelSize,
-          'height': itemPixelSize,
-          'color': '#fff',
-          'border': '1px',
-          'border-style': 'solid'
-        }).appendTo('.grid-container');
+        if (slot % 2 != 0) {
+          // Waypoint
+          $('<div id=' + "waypoint-" + waypointNr + '/>').css({
+            'position': 'absolute',
+            'top': (slot / 2 * size) + slot * 2,
+            'margin-left': (3 * lane * size) + (5 * lane) - (2 * size),
+            'width': size,
+            'height': size,
+            'color': '#fff',
+            'border': '1px',
+            'border-style': 'solid'
+          }).appendTo('.grid-container');
 
-        // Left Item Slot
-        $('<div id=' + "slot-left-" + dynamicSelector + '/>').css({
-          'position': 'absolute',
-          'top': 0,
-          'right': itemPixelSize,
-          'width': itemPixelSize,
-          'height': itemPixelSize,
-          'color': '#00BFFF',
-          'border': '1px',
-          'border-style': 'solid'
-        }).appendTo('#waypoint-' + dynamicSelector);
-
-        // Right Item Slot
-        $('<div id=' + "slot-right-" + dynamicSelector + '/>').css({
-          'position': 'absolute',
-          'top': 0,
-          'left': itemPixelSize,
-          'width': itemPixelSize,
-          'height': itemPixelSize,
-          'color': '#00BFFF',
-          'border': '1px',
-          'border-style': 'solid'
-        }).appendTo('#waypoint-' + dynamicSelector);
-      }
-    }
-
-    for(var item in itemPositions) {
-      if (itemPositions.hasOwnProperty(item)) {
-
-        var laneNr = getLaneNr(itemPositions[item]);
-        var slotNr = getSlotNr(itemPositions[item]);
-        var availableSlots = ['right', 'left'];
-        var randomSlot = availableSlots[Math.floor(Math.random() * (1 + 1))];
-        var position = laneNr + "-" + slotNr;
-
-        $('<div id=' + "item-" + position + '/>').css({
-          'position': 'absolute',
-          'top': 2,
-          'left': 2,
-          'width': itemPixelSize - 6,
-          'height': itemPixelSize - 6,
-          'color': '#3CB371',
-          'background-color': '#3CB371',
-          'border': '1px',
-          'border-style': 'solid',
-          'border-radius': 30
-        }).appendTo('#slot-' + randomSlot + '-' + position);
+          // Left Slot
+          $('<div id=' + "slot-" + slotNr + '/>').css({
+            'position': 'absolute',
+            'top': 0,
+            'right': size,
+            'width': size,
+            'height': size,
+            'color': '#00BFFF',
+            'border': '1px',
+            'border-style': 'solid'
+          }).appendTo('#waypoint-' + waypointNr);
+        } else {
+          // Right Slot
+          $('<div id=' + "slot-" + slotNr + '/>').css({
+            'position': 'absolute',
+            'top': 0,
+            'left': size,
+            'width': size,
+            'height': size,
+            'color': '#00BFFF',
+            'border': '1px',
+            'border-style': 'solid'
+          }).appendTo('#waypoint-' + waypointNr);
+        }
       }
     }
   }
 
-  function getLaneNr(absolutePosition) {
-    return Math.ceil(absolutePosition / slotsInLane);
-  }
+  function renderItems() {
+    var size = grid.getSlotPixelSize();
 
-  function getSlotNr(absolutePosition) {
-    if (absolutePosition % slotsInLane === 0) {
-      return slotsInLane;
-    } else {
-      return absolutePosition % slotsInLane;
+    for(var job in testJobs) {
+      if (testJobs.hasOwnProperty(job)) {
+        testJobs[job].forEach(function(slotNr) {
+          $('<div id=' + "item-" + slotNr + '/>').css({
+            'position': 'absolute',
+            'top': 2,
+            'left': 2,
+            'width': size - 6,
+            'height': size - 6,
+            'color': '#3CB371',
+            'background-color': '#3CB371',
+            'border': '1px',
+            'border-style': 'solid',
+            'border-radius': 30
+          }).appendTo('#slot-' + slotNr);
+        })
+      }
     }
   }
+};
 
-  function getRandomPosition() {
-    return Math.floor(Math.random() * (itemsTotal - 1) + 1);
-  }
-
-  return this;
-}(jQuery));
