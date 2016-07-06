16cca9737c465c64d98abbe16b7b5414540eda05
git <jkooper@gmx.net>
2016-07-06 21:31:53 +0200

    adds initial calculation of distances

--- app/js/index.controller.js
+++ app/js/index.controller.js
@@ -21,16 +21,16 @@ class IndexController {
   }
 
   generateJobs() {
-    var generatedData = "name,item1,item2,item3,item4,item5,item6\n";
+    var generatedData = 'name,groupId,item1,item2,item3,item4,item5,item6\n';
     for(var i=1;i<=10;i++){
       var items = Math.round(Math.random() * 3) + 3;
 
-      generatedData += "customer"+i+",";
+      generatedData += 'customer'+i+','+i+',';
 
       for(var j = 0; j<items;j++){
         generatedData += Math.round(Math.random()*63) + ',';
       }
-      generatedData = generatedData.substring(0,generatedData.length-1) + "\n";
+      generatedData = generatedData.substring(0,generatedData.length-1) + '\n';
     }
     generatedData = generatedData.substring(0,generatedData.length-1);
     document.getElementById('job-container').value = generatedData;
@@ -38,10 +38,35 @@ class IndexController {
 
   optimizeJobs() {
     var jobObj = this.processData(document.getElementById('job-container').value);
+    var slotsInLane = 12;//grid.getSlotsInLane();
+
+    var costTraversing = 0.5;
+    var costPerLane = 1.0;
+    var currentLane = 0;
+    //for(var singleJobObj of jobObj) {
+
+    for(var i=1; i<=Object.keys(jobObj).length;i++) {
+      var singleJobObj = jobObj[i];
+
+        var distance = 0;
+        for(var j=0;j<singleJobObj.items.length;j++) {
+          var singleItem = singleJobObj.items[j];
+
+          var goToLane = singleItem % slotsInLane;
+          distance += (goToLane-currentLane)*costTraversing;
+          if(goToLane != currentLane){
+            distance += costPerLane;
+          }
+          currentLane = goToLane;
+          jobObj[i].jobDistance = distance;
+          // have to add driving back to source
+      }
+    }
+
     console.log(jobObj);
-    // continue implementing
   }
 
+ 
   processData(allText) {
   var allTextLines = allText.split(/\r\n|\n/);
   var headers = allTextLines[0].split(',');
@@ -50,9 +75,17 @@ class IndexController {
   for (var i=1; i<allTextLines.length; i++) {
     var data = allTextLines[i].split(',');
     var tarr = {};
+    tarr['items'] = [];
 
     for (var j=0; j<data.length; j++) {
-      tarr[headers[j]] = data[j];
+
+      if(headers[j].search('item') != -1) {
+        tarr['items'].push(data[j]);
+        tarr['items'].sort(function(a, b){return a-b});
+      }
+      else{
+        tarr[headers[j]] = data[j];
+      }
     }
     obj[i] = tarr;
   }
